9634382f30db9ef6bf46481435c609a734d7fa01
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-11 21:54:06 +0000

    Added MySQL db; created template files; added add.php

--- index.php
+++ index.php
@@ -1,47 +1,16 @@
-<?php
-
-$shoes = [
-    [
-        'id' => 1,
-        'name' => 'Hiking boots',
-        'distance' => 3.2
-    ],
-    [
-        'id' => 2,
-        'name' => 'Running shoes',
-        'distance' => 2.5
-    ],
-    [
-        'id' => 3,
-        'name' => 'Casual sneakers',
-        'distance' => 12
-    ]
-]
-
-?>
+<?php include ('config/database.php');?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>Boot Tracker</title>
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <h2>Boot Tracker</h2>
-            <a href=""><img id="add-icon" src="img/circle-plus-solid.svg" alt=""></a>
-        </div>
+
+    <?php include ('templates/header.php');?>
 
         <?php foreach($shoes as $shoe) { ?>
             <div class="card">
             <img class="shoe-icon" src="img/shoe-prints-solid.svg" alt="">
-            <h3><?php echo htmlspecialchars($shoe['name']); ?></h3>
+            <h3><?php echo htmlspecialchars($shoe->name); ?></h3>
             <div class="distance-display">
-                <h1 class="distance-number"><?php echo htmlspecialchars($shoe['distance']); ?></h1>
+                <h1 class="distance-number"><?php echo htmlspecialchars($shoe->distance); ?></h1>
                 <h4 class="distance-unit">miles</h4>
             </div>
             <div class="card-controls">
@@ -51,6 +20,6 @@ $shoes = [
         </div>
         <?php } ?>
 
-    </div>
-</body>
+    <?php include ('templates/footer.php');?>
+
 </html>
---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Boot Tracker</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>Boot Tracker</h2>
+            <a href=""><img id="add-icon" src="img/circle-plus-solid.svg" alt=""></a>
+        </div>
