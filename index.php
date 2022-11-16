0fd2fc86ef882a3863a84423f9191e7eba733657
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-16 21:57:43 +0000

    Added dotenv package

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<?php include ('config/database.php');?>
+<?php 
 
-<?php
+use Dotenv\Dotenv;
+
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dsn = 'mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'];
+$pdo = new PDO($dsn, $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
 $stmt = $pdo->prepare('SELECT * FROM shoes');
 $stmt->execute();
 $shoes = $stmt->fetchAll(); 
+
 ?>
 
 <!DOCTYPE html>
