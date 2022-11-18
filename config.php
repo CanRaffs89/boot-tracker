b7a3063f293254f84993e5b3d0a944b92ad66497
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-18 21:58:55 +0000

    Changed connection

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = 'mysql:host=containers-us-west-119.railway.app;dbname=railway';
+$dsn = 'mysql:host=containers-us-west-119.railway.app;port=6025;dbname=railway';
 $pdo = new PDO($dsn, 'root', 'RrhIlNGY8bkjkuiOuty0');
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
