7bf125e64dabf4e763dbfd6310b30fbb512731d8
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-18 20:32:26 +0000

    Changed env variable names

--- config.php
+++ config.php
@@ -7,8 +7,8 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$dsn = 'mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'];
-$pdo = new PDO($dsn, $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+$dsn = 'mysql:host=' . $_ENV['MYSQLHOST'] . ';dbname=' . $_ENV['MYSQLDATABASE'];
+$pdo = new PDO($dsn, $_ENV['MYSQLUSER'], $_ENV['MYSQLPASSWORD']);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
 ?>
