ddc371a5269db09cdecdf4db7b58150df8d5a5d1
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-18 22:04:28 +0000

    Changed connection

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
 
-$dsn = 'mysql:host=containers-us-west-119.railway.app;port=6025;dbname=railway';
-$pdo = new PDO($dsn, 'root', 'RrhIlNGY8bkjkuiOuty0');
+use Dotenv\Dotenv;
+
+require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dsn = 'mysql:host=' . $_ENV['MYSQLHOST'] . ';port=' . $_ENV['MYSQLPORT'] . ';dbname=' . $_ENV['MYSQLDATABASE'];
+$pdo = new PDO($dsn, $_ENV['MYSQLUSER'], $_ENV['MYSQLPASSWORD']);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
 ?>
