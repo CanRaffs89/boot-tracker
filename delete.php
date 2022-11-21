3f6fcaf7e3ecce0c934407a18cddf309eae8d0e6
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-11-21 19:27:43 +0000

    Changed  header redirect

--- delete.php
+++ delete.php
@@ -1,10 +1,16 @@
-<?php include ('config.php');?>
-
 <?php 
+
+ob_start();
+
+include ('config.php');
 
 if(isset($_GET['id'])) {
     $stmt = $pdo->prepare('DELETE FROM shoes WHERE id = ?');
     $stmt->execute([$_GET['id']]);
+    header('Location: index.php');
 }
+
+ob_end_flush();
+exit;
 
 ?>
